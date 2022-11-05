c11feaa1d92dec5a91e7a1efe163d55f135cc3e2
Sebastien Thibert <sebastien.thibert@marks-and-spencer.com>
2022-11-05 09:21:06 +0000

    Initial Commit

---
+++ Mineswep/MineswepApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineswepApp.swift
+//  Mineswep
+//
+//  Created by Sebastien Thibert on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MineswepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
